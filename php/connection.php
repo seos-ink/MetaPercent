b479983513da8706d0a3e550b09d38ce6af1aefd
Saulo Santos <166402067+seos-ink@users.noreply.github.com>
2025-03-17 11:03:35 -0300

    Add files via upload

--- php/connection.php
+++ php/connection.php
@@ -1,14 +1,20 @@
 <?php
-$servername = "";
-$username = "###";
-$password = "###";
-$dbname = "###";
+$servername = "br612.hostgator.com.br";
+$username = "hubsap45_usrmetaper";
+$password = "f!R3@2o2XXV";
+$dbname = "hubsap45_bd_metaper";
 
-// CRIA A CONEXÃO DO BANCO DE DADOS 
+// Ativa relatórios de erros detalhados
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+// CRIA A CONEXÃO COM O BANCO DE DADOS 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// VERIFICA SE A CONXÃO ESTÁ CERTA E CASO HAJA ERRO ELE MOSTRA QUAL FOI 
+// VERIFICA SE A CONEXÃO FOI ESTABELECIDA COM SUCESSO
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
+
+// Exemplo de uso de conexão (como uma consulta)
+echo "Conexão bem-sucedida!";
 ?>
